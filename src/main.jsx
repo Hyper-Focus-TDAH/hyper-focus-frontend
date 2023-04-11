84fb70316d082d4c3dc0ccd35d8d1254488c4328
david-sds <dave.sathler@gmail.com>
2023-04-11 20:23:21 -0300

    i18n added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 import { RouterProvider } from "react-router-dom";
-import router from './router';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/fonts/littlepea/LittlePea-Demo-Regular.ttf'
+import { Wrapper } from "./components/wrapper";
+import router from "./router";
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Wrapper>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Wrapper>
+);
