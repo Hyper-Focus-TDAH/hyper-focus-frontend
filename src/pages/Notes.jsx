5dbca8925207645b093644782b652d7d3f0d2af5
david-sds <dave.sathler@gmail.com>
2023-04-15 20:51:36 -0300

    notes++

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,10 +1,15 @@
-import { Card, Form } from "react-bootstrap";
+import { Card, Form, CloseButton, Button } from "react-bootstrap";
 
 import { useState } from "react";
 
-import { t } from "../i18n/translate";
+import { useT } from "../i18n/translate";
+
+import { IconContext } from "react-icons";
+import { BsPlus } from "react-icons/bs";
 
 function Notes() {
+  const t = useT();
+
   const [noteText, setNoteText] = useState("");
   const [notes, setNotes] = useState([]);
 
@@ -12,27 +17,50 @@ function Notes() {
     setNoteText(event.target.value);
   }
 
-  const handleKeyDown = (event) => {
+  function handleKeyDown(event) {
     if (event.key === "Enter") {
-      const newNote = { id: Math.random().toString(), text: noteText };
-      setNoteText("");
-      setNotes((oldNotes) => {
-        return [newNote, ...oldNotes];
-      });
+      addNote();
     }
-  };
+  }
+
+  function addNote() {
+    if (noteText.length === 0) {
+      return;
+    }
+
+    const newNote = { id: Math.random().toString(), text: noteText };
+
+    setNotes((oldNotes) => {
+      const newNotes = [newNote, ...oldNotes];
+      return newNotes;
+    });
+
+    setNoteText("");
+  }
+
+  function removeNote(id) {
+    setNotes(notes.filter((note) => note.id !== id));
+  }
 
   return (
     <>
-      <p>{t("NOTES")}</p>
-      <Form.Control
-        value={noteText}
-        onChange={handleNoteTextChange}
-        onKeyDown={handleKeyDown}
-      />
+      <div className="p-3 d-flex justify-content-between align-items-center">
+        <Form.Control
+          value={noteText}
+          onChange={handleNoteTextChange}
+          onKeyDown={handleKeyDown}
+          placeholder={t("EXAMPLE_ADD_NOTE")}
+        />
+        <Button className="d-flex justify-content-center align-items-center ms-2" onClick={addNote}>
+          <BsPlus style={{fontSize: '25px'}} />
+        </Button>
+      </div>
       <div className="d-flex flex-wrap">
         {notes.map((note) => (
-          <Card className='m-1 p-2' key={note.id}>{note.text}</Card>
+          <Card className="m-1 p-2" key={note.id}>
+            {note.text}
+            <CloseButton onClick={() => removeNote(note.id)} />
+          </Card>
         ))}
       </div>
     </>
