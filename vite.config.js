256cb93fe990ec35f42796cfd8d5ce44a04e9eeb
Pedro Henrique Moreira <pedro.moreira@aoatechpartners.com.br>
2023-11-11 18:07:47 -0300

    fix(port): change app port

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as process from 'process';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 8081, // Set your desired port number here
+    port: 8081, // Set your desired port number here
   },
 });
