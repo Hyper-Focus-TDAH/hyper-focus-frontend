aa975119616c114fbefbe4b447712045ae86f38d
david-sds <dave.sathler@gmail.com>
2023-11-13 00:10:28 -0300

    fix(ajustes1): login layout adjustment

--- src/components/text-field/TextField.jsx
+++ src/components/text-field/TextField.jsx
@@ -1,10 +1,11 @@
-import { Form } from 'react-bootstrap';
+import { Form, InputGroup } from 'react-bootstrap';
 
 import { useT } from '../../i18n/translate';
 
 function TextField({
   id,
   type,
+  placeholder,
   intlKey,
   onChange,
   onBlur,
@@ -14,31 +15,36 @@ function TextField({
   disabled,
   as,
   accept,
+  append,
+  className = 'mb-4',
 }) {
   const t = useT();
 
   return (
-    <Form.Group className="position-relative mb-4">
-      {label && <Form.Label column>{label}</Form.Label>}
-      <Form.Control
-        id={id}
-        name={id}
-        type={type}
-        placeholder={t(intlKey)}
-        onChange={onChange}
-        onBlur={onBlur}
-        value={value}
-        accept={accept}
-        isInvalid={isInvalid}
-        disabled={disabled}
-        as={as}
-      />
-      <Form.Control.Feedback
-        style={{ display: 'flex', margin: '0' }}
-        type="invalid"
-      >
-        {isInvalid}
-      </Form.Control.Feedback>
+    <Form.Group className={`position-relative ${className}`}>
+      <InputGroup>
+        {append && <InputGroup.Text>{append}</InputGroup.Text>}
+        {label && <Form.Label column>{label}</Form.Label>}
+        <Form.Control
+          id={id}
+          name={id}
+          type={type}
+          placeholder={placeholder ?? t(intlKey)}
+          onChange={onChange}
+          onBlur={onBlur}
+          value={value}
+          accept={accept}
+          isInvalid={isInvalid}
+          disabled={disabled}
+          as={as}
+        />
+        <Form.Control.Feedback
+          style={{ display: 'flex', margin: '0' }}
+          type="invalid"
+        >
+          {isInvalid}
+        </Form.Control.Feedback>
+      </InputGroup>
     </Form.Group>
   );
 }
